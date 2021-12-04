a0b81053c657274a3af90267d4200f5fa2f86a17
Krystian <Krystian@example.com>
2021-12-04 09:20:27 +0100

    ISSUE -123 HTML template implementation

---
+++ template/main.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Moja gra</title>
+    </head>
+    <body>
+
+    <div></div>
+    <p>Kolejnosc zawodnikow:</p>
+    <ol>
+        <li>Zawodnik 1</li>
+        <li>Zawodnik 2 </li>
+    </ol>
+    <div>
+        <form method ="post" action ="/">
+            <p>Moj budzet: 1000 zł</p>
+            <select name="">
+                <option value="1">Zawodnik 1</option>
+                <option value="1">Zawodnik 2</option>
+                <option value="1">Zawodnik 3</option>
+            </select>
+
+            <select>
+            <option value="50">50 zł</option>
+            <option value="100">100 zł</option>
+            <option value="150">150 zł</option>
+
+            </select>
+            <input name="submit" type="submit" value="obstaw">
+            
+        </form>
+
+    </div>
+</body>
+</html>
